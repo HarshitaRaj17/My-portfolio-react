9d84dc5bb2b21c8a4e3241ee731e568dbab39b65
HarshitaRaj17 <88398133+HarshitaRaj17@users.noreply.github.com>
2023-04-16 13:25:33 +0530

    Updates

---
+++ precache-manifest.d28039d7f5aa29520b3f0aee50042734.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e80d713912ddfa6ad389",
+    "url": "/My-portfolio-react/static/css/main.8d164f4b.chunk.css"
+  },
+  {
+    "revision": "e80d713912ddfa6ad389",
+    "url": "/My-portfolio-react/static/js/main.e80d7139.chunk.js"
+  },
+  {
+    "revision": "e83d72c4726b2d399262",
+    "url": "/My-portfolio-react/static/css/1.3c86b137.chunk.css"
+  },
+  {
+    "revision": "e83d72c4726b2d399262",
+    "url": "/My-portfolio-react/static/js/1.e83d72c4.chunk.js"
+  },
+  {
+    "revision": "0184669c13e6d557d1ed",
+    "url": "/My-portfolio-react/static/js/runtime~main.0184669c.js"
+  },
+  {
+    "revision": "91fe04a6d06f8d28b5cf002bf3d87abb",
+    "url": "/My-portfolio-react/static/media/me.91fe04a6.jpg"
+  },
+  {
+    "revision": "d7d26c740151b1e099776df0d40ee158",
+    "url": "/My-portfolio-react/static/media/me1.d7d26c74.jpg"
+  },
+  {
+    "revision": "e448cefd3a4a525e297df6c88874728f",
+    "url": "/My-portfolio-react/static/media/me2.e448cefd.jpg"
+  },
+  {
+    "revision": "677d1e2b3981e3a396f6e9f960564282",
+    "url": "/My-portfolio-react/static/media/skill.677d1e2b.jpg"
+  },
+  {
+    "revision": "65a40e5eb912e76acbbbd21f9dbb0c5f",
+    "url": "/My-portfolio-react/static/media/art1.65a40e5e.jpg"
+  },
+  {
+    "revision": "61a0acddf25b218ccc9c21e12d58dcb3",
+    "url": "/My-portfolio-react/static/media/art2.61a0acdd.jpg"
+  },
+  {
+    "revision": "a446fa35cf8e92190740425646269d12",
+    "url": "/My-portfolio-react/static/media/art3.a446fa35.jpg"
+  },
+  {
+    "revision": "012d7bd07f071ce74a175d0365bf9741",
+    "url": "/My-portfolio-react/static/media/final-pc1.012d7bd0.jpg"
+  },
+  {
+    "revision": "ebb79dd33ba3975021106403f63095c9",
+    "url": "/My-portfolio-react/static/media/final-pc2.ebb79dd3.jpg"
+  },
+  {
+    "revision": "6506ee9dcee4c2f38366d911b12ec550",
+    "url": "/My-portfolio-react/index.html"
+  }
+];
